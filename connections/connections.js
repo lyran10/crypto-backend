bc5ecc639e92f20c651d58900ab9c916a4cb560b
lyran10 <liranramekar629@gmail.com>
2022-12-27 18:05:02 +0200

    added

--- connections/connections.js
+++ connections/connections.js
@@ -9,13 +9,13 @@ const db = knex({
   client:'pg',
   connection: {
     host:process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port:`${process.env.DB_PORT}`,
     user:process.env.DB_USER,
     password:process.env.DB_PASS,
     database:process.env.DB_NAME,
-    ssl:{rejectUnauthorized:true},
     idleTimeoutMillis: 0,
-    connectionTimeoutMillis : 0
+    connectionTimeoutMillis : 0,
+    ssl:{rejectUnauthorized:false},
   },
   acquireConnectionTimeout: 5000,
   pool: {
